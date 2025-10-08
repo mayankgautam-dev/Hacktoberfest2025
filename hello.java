0fa450fe93af694baaf88e9e346dd1f71c047ed3
Mayank <mayankgautam101@outlook.com>
2025-10-08 11:37:08 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int age = 25;
+        double price = 9.99;
+        boolean isJavaFun = true;
+        String name = "Alice";
+        System.out.println(name + " is " + age + " years old.");
+    }
+}
